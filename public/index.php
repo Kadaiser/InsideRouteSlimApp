edbb385d56c07237553a930b5061c6394682ecc7
Kadaiser <dvalbuen@ucm.es>
2022-04-03 10:42:04 +0100

    app rewrite to be launched via container

--- public/index.php
+++ public/index.php
@@ -1,18 +1,3 @@
 <?php
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Factory\AppFactory;
 
-require __DIR__ . '/../vendor/autoload.php';
-
-$displayErrorDetails = true;
-$app = AppFactory::create();
-
-$app->addErrorMiddleware(false, true, true);
-
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello Kada");
-    return $response;
-});
-
-$app->run();
+(require __DIR__ . '/../config/bootstrap.php')->run();
